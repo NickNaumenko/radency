db11e1824f8e0655d5c3181fef916eecb57f2c64
Nick <niiickolay@gmail.com>
2021-02-07 22:29:52 +0200

    Remove full name from unique fields list

--- src/services/employeeSchema.js
+++ src/services/employeeSchema.js
@@ -10,7 +10,7 @@ states.forEach(({ abbreviation, name }) => {
 const NOW = new Date();
 export const DATE_REGEXP = /^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$|^(0[1-9]|1[012])\/([123]0|[012][1-9]|31)\/(19[0-9]{2}|2[0-9]{3})$/;
 export const EMPLOYEE_REQUIRED_FIELDS = new Set(['fullName', 'phone', 'email']);
-export const EMPLOYEE_UNIQUE_FIELDS = ['fullName', 'phone', 'email'];
+export const EMPLOYEE_UNIQUE_FIELDS = ['phone', 'email'];
 const VALID_HAS_CHILDREN = new Set([true, false, null, undefined, 'true', 'false', 'null', 'undefined', '']);
 
 const employeeSchema = Nope.object().shape({
